4ef020b436473c8cb908d4be2e22669276edc52e
caiyanghui <caiyanghui>
2019-08-12 16:49:35 +0800

    本地第一次提交

---
+++ src/main/java/com/stone/giteemytest/GitEeMytestApplication.java
@@ -0,0 +1,13 @@
+package com.stone.giteemytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitEeMytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitEeMytestApplication.class, args);
+	}
+
+}
